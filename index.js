e04a814575bb2a3fee46fee78e065ba23fa70d6e
Blake Hardesty <ha1003218@email.ccbcmd.edu>
2019-01-11 15:49:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {};
 
-function updateObjectdWithKeyAndValue(object, key, value){
+function updateObjectWithKeyAndValue(object, key, value){
 return object.assign({}, object, {[key]: valfue});
 }
